a4419f198cc9c3c3231253eb3ed53b5e7d29f15a
AamirShaikh1 <aamirshaikh19933@gmail.com>
2017-08-07 16:43:10 +0530

    change code

--- javascript/my.js
+++ javascript/my.js
@@ -1,8 +1,8 @@
-var a = 40;
-var b = 20;
-var c = a-b;
+var a = 40
+var b = 20
+var c = a-b
 
 function myfunction() {
 	document.getElementById("demo").innerHTML = c;
 }
-myfunction();
+myfunction()
