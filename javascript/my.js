3480adc0a6c0ce9a2da02063dd775c555119a3eb
AamirShaikh1 <aamirshaikh19933@gmail.com>
2017-08-08 14:49:36 +0530

    date

--- javascript/my.js
+++ javascript/my.js
@@ -1,24 +1,28 @@
 	
-    function myfunction()
-    {
- var now = new Date();
+function myfunction()
+{
+	var now = new Date();
 
-var days = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+	var days = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+	// var days = ['Sunday', 'Monday'];
+	var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
 
-var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
 
-var date = ((now.getDate()<10) ? "0" : "")+ now.getDate();
+	var date = ( (now.getDate() < 10) ? "0" : "" ) + now.getDate();
+	console.log(date);
+	return;
 
-function fourdigits(number)	{
-	return (number < 1000) ? number + 1900 : number;
-								}
-today =  days[now.getDay()] + ", " +
-         months[now.getMonth()] + " " +
-         date + ", " +
-         (fourdigits(now.getYear())) ;
+	var time = setInterval( (now.getTime(), 1000) ? "0" : "" )+ now.getTime();
+
+	function fourdigits(number)	{
+		return (number < 1000) ? number + 1900 : number;
+	}
+
+	today =  days[now.getDay()] + ", " + months[now.getMonth()] + " " + date + ", " + (fourdigits(now.getYear())) ;
+	// time + "," +
 
 	document.getElementById("demo").innerHTML = today;
 	}
-	
 
-	
+
+
